604a73c3e2774cf4f40882fe502827de72626296
zvalant <96396989+zvalant@users.noreply.github.com>
2022-02-09 19:08:18 -0600

    Update user validation.py

--- user validation.py
+++ user validation.py
@@ -1,8 +1,8 @@
 def choice():
-    number = input("please choose a number between 0-10:")
-    if number.isdigit() == True and int(number) in range(0, 10):
-        return number
+    number = input("please choose a number between 0-10:") # get user input 
+    if number.isdigit() == True and int(number) in range(0, 10): # determine if input is digit and in range   
+        return number 
     else:
-        print("ERROR: please choose a number between 0-10")
+        print("ERROR: please choose a number between 0-10") # print error statement and return function to retry
         return choice()
 print(choice())
